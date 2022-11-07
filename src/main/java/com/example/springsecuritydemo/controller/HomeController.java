39892738b8b7b87b7e3115de58689c20c8add309
Kishan-Kumar-Zalavadia <kishankumar3131@gmail.com>
2022-11-07 14:25:56 +0530

    Initial commit

---
+++ src/main/java/com/example/springsecuritydemo/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.example.springsecuritydemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/home")
+    public String home(){
+        return "This is Home page";
+    }
+    @GetMapping("/admin")
+    public String admin(){
+        return "This is Admin page";
+    }
+}
